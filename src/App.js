d6d9943f343888df34992919ad3975b1b765f66e
Billy Paulsen <C-William.Paulsen@charter.com>
2019-08-15 12:44:13 -0500

    Adding some filler visuals for the month nav

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import MonthNav from "./components/monthNav";
 import MonthLog from "./components/monthLog";
+import "bulma/css/bulma.css";
 
 function App() {
   return (
--- src/components/monthNav.js
+++ src/components/monthNav.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 function MonthNav() {
-  return <div>This is the month nav</div>;
+  return (
+    <nav class="level is-mobile">
+      <div class="level-item has-text-centered">
+        <span class="icon">
+          <i class="fas fa-chevron-left" />
+        </span>
+      </div>
+      <div class="level-item has-text-centered">August</div>
+      <div class="level-item has-text-centered">
+        <span class="icon">
+          <i class="fas fa-chevron-right" />
+        </span>
+      </div>
+    </nav>
+  );
 }
 
 export default MonthNav;
